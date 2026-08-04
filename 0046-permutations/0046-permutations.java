fc11f7dbaf6e2de4cf9c994326214d8e816260a6
Poovalingam-17 <poovalingam24102005@gmail.com>
2026-08-04 11:05:49 +0530

    Time: 1 ms (87.15%), Space: 45 MB (98.73%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static void Permutation(int[]nums,List<List<Integer>> res,List<Integer> list,boolean[]arr){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!arr[i]){
+                arr[i]=true;
+                list.add(nums[i]);
+                Permutation(nums,res,list,arr);
+                arr[i]=false;
+                list.remove(list.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean []arr = new boolean [nums.length];
+        Permutation(nums,res,list,arr);
+        return res;
+    }
+}
