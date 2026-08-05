b4a169af86b5efc8cf5d7b6cb1efa4ba195e3aef
Poovalingam-17 <poovalingam24102005@gmail.com>
2026-08-05 14:49:06 +0530

    Update Code.java

--- Amazon/Minimum Operations to Make an Array Non-Decreasing/Code.java
+++ Amazon/Minimum Operations to Make an Array Non-Decreasing/Code.java
@@ -2,14 +2,21 @@ import java.util.*;
 class Main {
     public static int find(int []arr){
         int count = 0;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
-                while(arr[i]>arr[i+1]){
-                    arr[i+1]+=3;
-                    count++;
-                }
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1]>arr[i]){
+                int dif = (int)Math.ceil((double)(arr[i-1]-arr[i])/3);
+                arr[i]=arr[i-1]*dif;
+                count+=dif;
             }
         }
+        // for(int i=0;i<arr.length-1;i++){
+        //     if(arr[i]>arr[i+1]){
+        //         while(arr[i]>arr[i+1]){
+        //             arr[i+1]+=3;
+        //             count++;
+        //         }
+        //     }
+        // }
         return count;
     }
     public static void main(String[] args) {
