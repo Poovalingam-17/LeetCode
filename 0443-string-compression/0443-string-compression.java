ed2abdeb76fc5366351ab7f5726fb83b1536bb4a
Poovalingam-17 <poovalingam24102005@gmail.com>
2026-08-06 14:39:14 +0530

    Time: 2 ms (27.63%), Space: 45.3 MB (66.13%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for(int i=0;i<chars.length-1;i++){
+            if(chars[i]==chars[i+1]){
+                count++;
+            }
+            else{
+                sb.append(chars[i]);
+                if(count!=1){
+                    sb.append(String.valueOf(count));
+                }
+                count=1;
+            }
+        }
+        sb.append(chars[chars.length-1]);
+        if(count!=1){
+            sb.append(count);
+        }
+        for(int i=0;i<sb.length();i++){
+            chars[i]=sb.charAt(i);
+        }
+        return sb.length();
+    }
+}
