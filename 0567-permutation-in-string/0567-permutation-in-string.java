33123160e3941a830cea8b5d6b6a79422b9f9eb3
Poovalingam-17 <poovalingam24102005@gmail.com>
2026-08-20 11:05:10 +0530

    Time: 7 ms (68.76%), Space: 43.8 MB (85.47%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,20 +1,35 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int freq[]=new int [26];
-        for(char c:s1.toCharArray()){
-            freq[c-'a']++;
-        }
-        boolean found=false;
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            int freq1[]=new int[26];
-            for(int j=i;j<s1.length()+i;j++){
-                freq1[s2.charAt(j)-'a']++;
-                if(Arrays.equals(freq,freq1)){
-                       found=true;
-
-                }
+    public static boolean same(int[]arr1,int[]arr2){
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
             }
         }
-        return found;
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int []fre1 = new int[26];
+        int []fre2 = new int[26];
+        if(s1.length()>s2.length()){
+            return false;
+        }
+        int win = s1.length();
+        for(char ch : s1.toCharArray()){
+            fre1[ch-'a']++;
+        }
+        for(int i=0;i<win;i++){
+            fre2[s2.charAt(i)-'a']++;
+        }
+        if(same(fre1,fre2)){
+            return true;
+        }
+        for(int i=win;i<s2.length();i++){
+            fre2[s2.charAt(i)-'a']++;
+            fre2[s2.charAt(i-win)-'a']--;
+            if(same(fre1,fre2)){
+                return true;
+            }
+        }
+        return false;
     }
 }
