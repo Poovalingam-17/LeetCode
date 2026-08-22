42bec25b111e8ad4b1f5ce6aa3cc77d97c5a9e2d
Poovalingam-17 <poovalingam24102005@gmail.com>
2026-08-22 14:43:00 +0530

    Time: 9 ms (6.43%), Space: 46.5 MB (54.65%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length/2;i++){
+            for(int j=0;j<nums[2*i];j++){
+                list.add(nums[2*i+1]);
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
